42d002d91d721e4de0091cd3529f6cf197d8078e
jhoeller <jhoeller>
2003-04-23 15:39:44 +0000

    polishing and docs, dissolved XmlApplicationContextSupport

--- src/com/interface21/context/support/AbstractXmlApplicationContext.java
+++ src/com/interface21/context/support/AbstractXmlApplicationContext.java
@@ -25,7 +25,7 @@ import com.interface21.context.ApplicationContextException;
  * understood by an XMLBeanFactory
  * @see com.interface21.beans.factory.support.XmlBeanFactory
  * @author  Rod Johnson
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public abstract class AbstractXmlApplicationContext extends AbstractApplicationContext  {
 
@@ -34,7 +34,6 @@ public abstract class AbstractXmlApplicationContext extends AbstractApplicationC
 	//---------------------------------------------------------------------
 	/** Default BeanFactory for this context */
 	private ListableBeanFactory listableBeanFactory; 
-
 
 	//---------------------------------------------------------------------
 	// Constructors
@@ -77,15 +76,12 @@ public abstract class AbstractXmlApplicationContext extends AbstractApplicationC
 				throw new ApplicationContextException("IOException closing stream for XML document for " + identifier, ex);
 			}
 		}
-	}   // refresh
+	}
 	
-
-
 	/** Build URL mappings etc. from Document
 	 **/
 	private void parseDocument(Document doc) throws ApplicationContextException {
 		Element root = doc.getDocumentElement();
-		//log4jCategory.debug("Parsed XML configuration from well-formed '" + getURL() + "': processing document...");
 
 		try {
 			listableBeanFactory = new XmlBeanFactory(doc, getParent());
@@ -101,9 +97,8 @@ public abstract class AbstractXmlApplicationContext extends AbstractApplicationC
 			logger.error(mesg, ex);
 			throw new ApplicationContextException(mesg, ex);
 		}
-	}   // parseDocument
+	}
  
-
 	/** Return the default BeanFactory for this context
 	 * @return the default BeanFactory for this context
 	 */
@@ -118,5 +113,5 @@ public abstract class AbstractXmlApplicationContext extends AbstractApplicationC
 	 */
 	protected abstract InputStream getInputStreamForBeanFactory() throws IOException;
 	
-}	// class AbstractXMLApplicationContext
+}
  
