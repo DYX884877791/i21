85d31f8b0375cd12fa38c39c973e77455ab74570
luke_t <luke_t>
2003-05-01 15:28:14 +0000

    removed incorrect cutnpasted javadoc comments referring to ReflectionException

--- src/com/interface21/beans/TypeMismatchException.java
+++ src/com/interface21/beans/TypeMismatchException.java
@@ -6,22 +6,18 @@ import java.beans.PropertyChangeEvent;
 /**
  * Exception thrown on a type mismatch when trying to set a property
  * @author  Rod Johnson
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class TypeMismatchException extends PropertyAccessException {
-	
-	/**
-	 * Constructs an <code>ReflectionException</code> with the specified detail message.
-	 * @param msg the detail message.
-	 */
+
 	public TypeMismatchException(PropertyChangeEvent propertyChangeEvent, Class requiredType) {
 		super("Failed to convert property value of type [" + propertyChangeEvent.getNewValue().getClass().getName() + "] to required type [" + requiredType.getName() + "]", propertyChangeEvent);
 	}
-	
+
 	public TypeMismatchException(PropertyChangeEvent propertyChangeEvent, Class requiredType, Throwable t) {
 		super("Failed to convert property value of type [" + propertyChangeEvent.getNewValue().getClass().getName() + "] to required type [" + requiredType.getName() + "]", t, propertyChangeEvent);
 	}
-	
+
 }
 
 
