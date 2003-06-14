afd058d071471671259b6fb6db3a46b7df73f529
pawlakjp <pawlakjp>
2003-06-14 00:36:28 +0000

    *** empty log message ***

--- src/com/interface21/context/ApplicationContext.java
+++ src/com/interface21/context/ApplicationContext.java
@@ -36,9 +36,9 @@ import com.interface21.beans.factory.ListableBeanFactory;
  * own child context that is independent of that of any other servlet.
  * </ul>
  * @author Rod Johnson
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.7.2.1 $
  */
-public interface ApplicationContext extends MessageSource, ListableBeanFactory {
+public interface ApplicationContext extends MessageSource, ListableBeanFactory, ThemeSource {
 	
 	/**
 	 * Return the parent context, or null if there is no parent,
